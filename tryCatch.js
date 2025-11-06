aaad99f0c3703b79b3aadb80c3248eafd082942f
Manavvv07 <manavsingh29sk@gmail.com>
2025-11-06 16:54:24 +0530

    practice

---
+++ tryCatch.js
@@ -0,0 +1,18 @@
+try
+{
+    console.log("Beginning of test runs");
+
+    unicycle;
+
+    console.log("End of test runs");
+}
+catch(err)
+{
+    console.log("error has occured: " + err);
+}
+finally
+{
+    console.log("this will always run");
+}
+
+console.log("The execution continues");
