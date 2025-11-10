9470cad2006f25c2c22e21a15f7ea6ffb37d4495
Manavvv07 <manavsingh29sk@gmail.com>
2025-11-10 17:05:47 +0530

    Q1

---
+++ Practice question sheet/Q1.js
@@ -0,0 +1,11 @@
+var number1 = 10;
+let number2 = 20;
+const number3 = 30;
+
+var number1 = 15;
+//let number2 = 2; (cannot re-declare)
+//const number3 = 3;(cannot re-declare)
+
+number1 = 20;
+number2 = 25;
+//number3 = 19; (cannot re-ssign)
